f9dde80428cc5a68ebd81cf76e957abb7108db9e
Abhishek Dhawan <abhishekdhawan@berkeley.edu>
2017-02-20 16:53:10 -0800

    Edited explore.R

--- explore.R
+++ explore.R
@@ -25,7 +25,8 @@ names(tableOfErrors) = c("exp", "means", "standard_errors")
 
 tableOfErrors$exp = factpr(tableOfErrors$exp)
 
-# Error bars represent standard error of the mean
+# Error bars represent standard error of the mean by experiment
+
 ggplot(tableOfErrors, aes(x=exp, y=means)) + 
   geom_bar(position=position_dodge(), stat="identity") +
   geom_errorbar(aes(ymin=means-2*standard_errors, ymax=means+2*standard_errors),
@@ -34,6 +35,8 @@ ggplot(tableOfErrors, aes(x=exp, y=means)) +
 
 summary = trial[!is.na(error), .(m = mean(error), se = sd(error)/sqrt(.N)), by = .(exp, run)]
 
+# Error bars represent standard error of the mean by experiment and run
+
 ggplot(summary, aes(x=exp, y=m, fill = run)) + 
   geom_bar(position=position_dodge(), stat="identity") +
   geom_errorbar(aes(ymin=m-2*se, ymax=m+2*se),
@@ -41,9 +44,20 @@ ggplot(summary, aes(x=exp, y=m, fill = run)) +
                 position=position_dodge(.9))
 
 
-summaryOrientation = trial[!is.na(error), .(or = GetOrientation(stimulus), error, exp = factor(exp), run)]
+summaryOrientation = trial[!is.na(error), .(or = GetOrientation(stimulus), error, exp = factor(exp), run, id = sbjId)]
+
+# Trends accross experiments
 
 ggplot(summaryOrientation, aes(x = or, y = error, color = exp)) + geom_point(alpha = 0.2) + geom_smooth() + facet_grid(.~run)
 
-# Hist of stimulus orientation/angle, compare each subject to the average trend (facet_wrap(~subjectId))
+# Hist of stimulus orientation/angle
+
+ggplot(summaryOrientation, aes(x = or)) + geom_histogram(colour = "black", fill = "white", binwidth = 5)
+
+# Compare each subject to the average trend (facet_wrap(~subjectId))
+
+ggplot(summaryOrientation, aes(x = or, y = error, colour = run)) + geom_point(alpha = 0.2) + geom_smooth() + facet_wrap(~id)
+
+
+
 
